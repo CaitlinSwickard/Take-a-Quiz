b930b82f32ddb2e22876a06a1283062d9c41f6d6
Caitlin Swickard <caitlin6029@gmail.com>
2021-05-11 11:03:11 -0600

    addition to JavaScript code

--- script.js
+++ script.js
@@ -12,21 +12,90 @@
 
 
 const startButton = document.getElementById('start-btn')
+const questionContainerElement = document.getElementById('question-container')
+const questionElement = document.getElementById('question')
+const answerButtonElement = document.getElementById('answer-buttons')
 
 
-startButton.addEventListener('click', startGame)
+let currentQuestion, currentQuestionIndex
+
+
+startButton.addEventListener('click', startQuiz)
+
+
+
+// function to start the quiz
+function startQuiz() {
+  console.log('started');
+  startButton.classList.add('hide');
+  currentQuestion = questions;
+  currentQuestionIndex = 0
+  questionContainerElement.classList.remove('hide');
+  setNextQuestion()
+}
+
+
+
+function setNextQuestion() {
+  showQuestions(currentQuestion);
+}
+
+
+
+function showQuestions(question) {
+  questionElement.innerText = questions.question
+  question.answers.forEach(answer => {
+    const button = document.createElement('button')
+    button.innerText = answer.text
+    button.classList.add('btn')
+    if (answer.correct) {
+      button.dataset.correct = answer.correct
+    }
+  })
+}
 
 
 
 
-function startGame {
+function selectAnswer() {
 
 }
 
-function setNextQuestion {
-
-}
-
-function selectAnswer {
-
-}
+let questions = [
+  {
+    question: 'What is 1 + 1?',
+    answer: [
+      { text: '11', correct: false },
+      { text: '111', correct: false },
+      { text: '2', correct: true },
+      { text: '4', correct: false },
+    ]
+  },
+  {
+    question: 'What is 3 + 3?',
+    answer: [
+      { text: '6', correct: true },
+      { text: '9', correct: false },
+      { text: '3', correct: false },
+      { text: '33', correct: false },
+    ]
+  },
+  {
+    question: 'What is 6 + 6?',
+    answer: [
+      { text: '66', correct: false },
+      { text: '12', correct: true },
+      { text: '9', correct: false },
+      { text: '3', correct: false },
+    ]
+  },
+  {
+    question: 'What is 4 + 4?',
+    answer: [
+      { text: '7', correct: false },
+      { text: '8', correct: true },
+      { text: '9', correct: false },
+      { text: '10', correct: false },
+    ]
+  }
+];
